f79ce24ed7bed1f1d474ad4c50f9e4d75dbba81d
LyleBrown010 <lylebrown010@gmail.com>
2023-05-23 11:22:31 +0200

    message done too

--- BMI.js
+++ BMI.js
@@ -32,16 +32,16 @@ function calculate(){
     let result = (weight / (height * height) * 10000)
     document.getElementById('result').innerHTML = result.toFixed(2)
 
-    if(result < 18.5){
-        document.getElementById('category').value  = 'Underweight'
+    if(result <= 18.5){
+        document.getElementById('result').innerHTML = result.toFixed(2) + " " + 'Underweight'
     }
-    else if (result >= 18.5 && bmi <= 24.9){
-        document.getElementById('category').value  = 'normal weight'
+    else if (result > 18.5 && result <= 24.9){
+        document.getElementById('result').innerHTML = result.toFixed(2) + " " + 'Normal weight'
     }
-    else if (result >= 25 && bmi <= 39.9){
-        document.getElementById('category').value  = 'Overweight'
+    else if (result > 25 && result <= 39.9){
+        document.getElementById('result').innerHTML = result.toFixed(2) + " " + 'Overweight'
     }
     else {
-        document.getElementById('category').value  = 'obese'
+        document.getElementById('result').innerHTML = result.toFixed(2) + " " + 'Obese'
     }
 }
